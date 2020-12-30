93b692c0dcfe8d10a0a14b105deb09436da539a6
berinasmajovic <berina.smajovic1@gmail.com>
2020-12-30 16:30:15 +0100

    first commit

---
+++ login.js
@@ -0,0 +1,65 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'password',
+	database : 'secretsantasystem'
+});
+
+var app = express();
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/images'));
+
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.get('/employee-homepage', function(request, response) {
+	response.sendFile(path.join(__dirname + '/employee-homepage.html'));
+});
+
+app.post('/auth', function(request, response) {
+	var email = request.body.email;
+	var password = request.body.password;
+	if (email && password) {
+		connection.query('SELECT * FROM employees WHERE email = ? AND password = ?', [email, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.email = email;
+				response.redirect('/employee-homepage');
+			} else {
+				response.send('Incorrect Email and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Email and Password!');
+		response.end();
+	}
+});
+
+app.get('/employee-homepage', function(request, response) {
+	if (request.session.loggedin) {
+        response.send('Welcome back, ' + request.session.email + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+app.listen(3000);
